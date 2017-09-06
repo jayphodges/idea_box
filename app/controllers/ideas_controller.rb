7bb287064d88f4f76ba2e199196a123bd3da1eb3
jayphodges <jayphodges@gmail.com>
2017-09-06 15:06:57 -0600

    Initial CRUD completed

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,28 +1,21 @@
 class IdeasController < ApplicationController
   before_action :set_idea, only: [:show, :edit, :update, :destroy]
 
-  # GET /ideas
-  # GET /ideas.json
   def index
     @ideas = Idea.all
   end
 
-  # GET /ideas/1
-  # GET /ideas/1.json
   def show
   end
 
-  # GET /ideas/new
   def new
+    @categories = Category.all
     @idea = Idea.new
   end
 
-  # GET /ideas/1/edit
   def edit
   end
 
-  # POST /ideas
-  # POST /ideas.json
   def create
     @idea = Idea.new(idea_params)
 
@@ -37,8 +30,6 @@ class IdeasController < ApplicationController
     end
   end
 
-  # PATCH/PUT /ideas/1
-  # PATCH/PUT /ideas/1.json
   def update
     respond_to do |format|
       if @idea.update(idea_params)
@@ -51,8 +42,6 @@ class IdeasController < ApplicationController
     end
   end
 
-  # DELETE /ideas/1
-  # DELETE /ideas/1.json
   def destroy
     @idea.destroy
     respond_to do |format|
@@ -62,13 +51,13 @@ class IdeasController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+
     def set_idea
       @idea = Idea.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def idea_params
       params.require(:idea).permit(:content, :category_id)
     end
+
 end
